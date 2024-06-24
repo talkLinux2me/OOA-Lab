3c7263a9298739bbceef63dbc8143d88ff6edd39
talkLinux2me <167791277+talkLinux2me@users.noreply.github.com>
2024-06-24 19:53:31 -0400

    part two

--- app.js
+++ app.js
@@ -16,3 +16,19 @@ const adventurer = {
       console.log(`${this.name} rolled a ${result}.`);
     },
   };
+
+  //Part Two
+class Character {
+    constructor(name) {
+      this.name = name;
+      this.health = 100;
+      this.inventory = [];
+    }
+  
+    roll(mod = 0) {
+      const result = Math.floor(Math.random() * 20) + 1 + mod;
+      console.log(`${this.name} rolled a ${result}.`);
+      return result;
+    }
+  }
+  
