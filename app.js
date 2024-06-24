6d80cac1cafe700d812ad7c19227f517f8cddd66
talkLinux2me <167791277+talkLinux2me@users.noreply.github.com>
2024-06-24 19:58:46 -0400

    finishing part 4

--- app.js
+++ app.js
@@ -79,4 +79,29 @@ class Adventurer extends Character {
               console.log(`${this.name} health is now ${this.health}`);
             }
           }
-        
+          class Companion extends Character {
+            constructor(name, type) {
+              super(name);
+          
+              this.type = type;
+            }
+          
+            assist() {
+              console.log(`${this.name} is helping `);
+              super.roll();
+            }
+          }
+          
+          // const robin = new Adventurer("Robin", "Scout");
+          // robin.inventory = ["sword", "potion", "artifact"];
+          // robin.companion = new Companion("Leo");
+          // robin.companion.type = "Cat";
+          // robin.companion.companion = new Companion("Frank");
+          // robin.companion.companion.type = "Flea";
+          // robin.companion.companion.inventory = ["small hat", "sunglasses"];
+          
+          // robin.scout();
+          // robin.companion.assist();
+          // robin.companion.companion.assist();
+          
+          
